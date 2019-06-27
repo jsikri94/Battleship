88f9fac68fefb9777a539b56f9fecaea1393677c
Jaahanavee Sikri <jaahanaveesikri@gmail.com>
2019-06-27 08:45:39 -0700

    Added firebase hosting capabilities

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,11 +7,33 @@ interface IBoardProps {
 export default class Board extends React.Component<IBoardProps> {
   render() {
     const board = [];
-
+    const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    const firstrow = [];
+    firstrow.push(
+      <Square isOpponentsBoard={this.props.isOpponentsBoard} value={"BS"} />
+    );
+    for (let i = 0; i < 10; i++) {
+      firstrow.push(
+        <Square
+          isOpponentsBoard={this.props.isOpponentsBoard}
+          value={letters[i]}
+        />
+      );
+    }
+    board.push(<div>{firstrow}</div>);
     for (let i = 0; i < 10; i++) {
       const row = [];
+      const val = i + 1;
+      row.push(
+        <Square
+          isOpponentsBoard={this.props.isOpponentsBoard}
+          value={val.toString()}
+        />
+      );
       for (let j = 0; j < 10; j++) {
-        row.push(<Square isOpponentsBoard={this.props.isOpponentsBoard} />);
+        row.push(
+          <Square isOpponentsBoard={this.props.isOpponentsBoard} value="~" />
+        );
       }
       board.push(<div>{row}</div>);
     }
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -3,42 +3,62 @@ import * as React from "react";
 interface ISquareState {
   color: string;
   bgColor: string;
+  play: boolean;
 }
 interface ISquareProps {
   isOpponentsBoard?: boolean;
+  value?: string;
 }
 
 class Square extends React.Component<ISquareProps, ISquareState> {
   constructor(props: ISquareProps) {
     super(props);
-    this.state = {
-      color: "#1E7EF8",
-      bgColor: "#1E7EF8"
-    };
+    if (this.props.value === "~") {
+      this.state = {
+        color: "#1e7ef8",
+        bgColor: "#1e7ef8",
+        play: false
+      };
+    } else {
+      this.state = {
+        color: "white",
+        bgColor: "white",
+        play: false
+      };
+    }
   }
   handleClick = () => {
-    if (this.props.isOpponentsBoard) {
-      if (this.state.bgColor === "white") {
-        this.setState({
-          color: "red",
-          bgColor: "red"
-        });
-      } else if (this.state.bgColor === "red") {
-        this.setState({
-          color: "#1E7EF8",
-          bgColor: "#1E7EF8"
-        });
+    if (this.props.value === "~") {
+      if (this.props.isOpponentsBoard) {
+        if (this.state.bgColor === "#1e7ef8") {
+          this.setState({
+            color: "red",
+            bgColor: "red"
+          });
+        } else if (this.state.bgColor === "red") {
+          this.setState({
+            color: "white",
+            bgColor: "white"
+          });
+        } else if (this.state.bgColor === "white") {
+          this.setState({
+            color: "#1e7ef8",
+            bgColor: "#1e7ef8"
+          });
+        }
       } else {
-        this.setState({
-          color: "white",
-          bgColor: "white"
-        });
+        if (this.state.bgColor === "#1e7ef8") {
+          this.setState({
+            color: "grey",
+            bgColor: "grey"
+          });
+        } else if (this.state.bgColor === "grey") {
+          this.setState({
+            color: "#1e7ef8",
+            bgColor: "#1e7ef8"
+          });
+        }
       }
-    } else {
-      this.setState({
-        color: "grey",
-        bgColor: "grey"
-      });
     }
   };
   render() {
@@ -53,7 +73,9 @@ class Square extends React.Component<ISquareProps, ISquareState> {
           height: "30px",
           width: "30px"
         }}
-      />
+      >
+        {this.props.value}
+      </button>
     );
   }
 }
